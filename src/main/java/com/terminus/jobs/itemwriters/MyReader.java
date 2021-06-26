966321c101c006236d8ac9e016203dce6fd6cb0c
meiyukai <1328667242@qq.com>
2021-06-26 19:47:11 +0800

    learned FlatFileItemWriter

---
+++ src/main/java/com/terminus/jobs/itemwriters/MyReader.java
@@ -0,0 +1,52 @@
+package com.terminus.jobs.itemwriters;
+
+import com.terminus.pojo.Student;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.Order;
+import org.springframework.batch.item.database.support.MySqlPagingQueryProvider;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MyReader  {
+
+    private DataSource datasource;
+
+    public MyReader(DataSource datasource) {
+        this. datasource = datasource;
+    }
+
+
+
+    public ItemReader obtainReader(){
+        JdbcPagingItemReader<Student> jdbcReader = new JdbcPagingItemReader<>();
+        jdbcReader.setDataSource(datasource);
+        jdbcReader.setRowMapper(getRowMapper());
+        jdbcReader.setQueryProvider(getProvider());
+        return jdbcReader;
+    }
+
+    public RowMapper<Student> getRowMapper(){
+        return (resultSet, i) -> {
+            Student student = new Student();
+            student.setId(resultSet.getInt("id"));
+            student.setAge(resultSet.getInt("age"));
+            student.setName(resultSet.getString("name"));
+            student.setAddress(resultSet.getString("address"));
+            return  student;
+        };
+    }
+
+    public MySqlPagingQueryProvider getProvider(){
+        MySqlPagingQueryProvider provider = new MySqlPagingQueryProvider();
+        provider.setSelectClause("select id,age,name,address");
+        provider.setFromClause("from student");
+        Map<String, Order> orderMap = new HashMap<>();
+        orderMap.put("age",Order.ASCENDING);
+        provider.setSortKeys(orderMap);
+        return provider;
+    }
+}
---
+++ src/main/java/com/terminus/jobs/itemwriters/filereaders/ItemFileWriterDemo.java
@@ -0,0 +1,82 @@
+package com.terminus.jobs.itemwriters.filereaders;
+
+import com.terminus.jobs.itemwriters.MyReader;
+import com.terminus.pojo.Student;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.transform.LineAggregator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import javax.sql.DataSource;
+import java.util.UUID;
+
+/**
+ * 从数据中读取数据写入文件中
+ * by <author>meiyukai</author>
+ * at <Date>2021-06-26</Date>
+ */
+
+@Configuration
+@EnableBatchProcessing
+public class ItemFileWriterDemo {
+
+    @Autowired
+    private DataSource dataSource;
+
+    /*@Value("classpath:itemfilewriter.txt")
+    private Resource resource;*/
+
+    private Resource resource = new FileSystemResource("/Users/meiyukai/Desktop/testwriter.txt");
+
+    @Bean ItemReader<Student> reader(){
+        return new MyReader(dataSource).obtainReader();
+    }
+
+    @Bean
+    public ItemWriter<Student> writer(){
+        FlatFileItemWriter<Student> fwriter = new FlatFileItemWriter<>();
+        fwriter.setResource(resource);
+        fwriter.setLineAggregator(getLineAggregator());
+        return fwriter;
+    }
+
+    @Bean
+    public Step step1(StepBuilderFactory stepBuilderFactory, ItemReader reader, ItemWriter writer){
+        return stepBuilderFactory.get("step1")
+                .chunk(2)
+                .reader(reader)
+                .writer(writer)
+                .build();
+    }
+
+    @Bean
+    public Job itemFileWriterJob(JobBuilderFactory jobBuilderFactory, Step step1){
+        return jobBuilderFactory.get("itemFileWriterJob - " + UUID.randomUUID())
+                .start(step1)
+                .build();
+
+    }
+
+    public LineAggregator<Student> getLineAggregator(){
+        return  student->{
+            StringBuffer studentBuffer = new StringBuffer();
+            studentBuffer.append(student.getId()).append(",")
+                    .append(student.getAge()).append(",")
+                    .append(student.getName()).append(",")
+                    .append(student.getAddress());
+            return studentBuffer.toString();
+        };
+    }
+
+
+}
