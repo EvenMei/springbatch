5c107b06c50da1da989bf465812208d286727069
meiyukai <1328667242@qq.com>
2021-06-26 16:29:15 +0800

    learned handleException in ItemReader by implements ItemStreamReader

---
+++ src/main/java/com/terminus/jobs/itemreaders/exceptionhandle/MyWriter.java
@@ -0,0 +1,16 @@
+package com.terminus.jobs.itemreaders.exceptionhandle;
+
+import com.terminus.pojo.Student;
+import org.springframework.batch.item.ItemWriter;
+
+import java.util.List;
+
+public class MyWriter  implements ItemWriter<Student> {
+    @Override
+    public void write(List<? extends Student> list) throws Exception {
+        System.out.println("------------------->  start write !" );
+        for (Student student : list){
+            System.out.println("student   :  " + student);
+        }
+    }
+}
